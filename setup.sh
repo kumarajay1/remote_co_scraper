3e69e8d221f5994be424e411db708b74a2742dd0
kumarajay1 <majaykumar.iiitbasar@gmail.com>
2020-01-21 16:58:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo apt-get install python3 -y
-sudo apt-get install python3-pip
+sudo apt-get install python3-setuptools -y
+sudo apt-get install python3-pip -y
 sudo pip3 install pandas
 sudo pip3 install selenium
 sudo pip3 install beautifulsoup4
