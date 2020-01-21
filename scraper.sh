44a8cf602dda2f966bcb1dcfd3cc9bd95a641e4c
kumarajay1 <majaykumar.iiitbasar@gmail.com>
2020-01-21 15:53:03 +0530

    Create scraper.sh

---
+++ scraper.sh
@@ -0,0 +1,7 @@
+export PATH=$PATH:/home/ubuntu/angle_co_scrape
+echo "scraper starting"
+python3 ./remote_co_scraper/remote_co_scraper.py 
+sleep 3 
+echo "Scraped the job details"
+python3 ./remote_co_scraper/insert_into_db.py
+echo "Inserted into DB"
