affc894238da999f5389ede64ac92409ca1c8754
kumarajay1 <majaykumar.iiitbasar@gmail.com>
2020-01-21 17:10:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get install python3 -y
 sudo apt-get install python3-setuptools -y
+sudo apt-get install firefox
 sudo apt-get install python3-pip -y
 sudo pip3 install pandas==0.23.4
 sudo pip3 install selenium
