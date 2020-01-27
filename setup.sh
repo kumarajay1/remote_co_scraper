40cb0d22d4e67c226f44d4bea77888c25da2ba7c
kumarajay1 <majaykumar.iiitbasar@gmail.com>
2020-01-27 11:59:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 sudo apt-get update
+sudo apt-get upgrade
+sleep 10
 sudo apt-get install python3 -y
 sudo apt-get install jq -y
 sudo apt-get install python3-setuptools -y
-sudo apt-get install firefox -y
 sudo apt-get install python3-pip -y
 sudo pip3 install pandas==0.23.4
 sudo pip3 install selenium
 sudo pip3 install beautifulsoup4
 sudo pip3 install lxml
 sudo pip3 install cassandra-driver
+sudo apt-get install firefox -y
