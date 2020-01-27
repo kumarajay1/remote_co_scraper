c9b2b243c650a57d6c6f5fec9a8546ac6874194a
kumarajay1 <majaykumar.iiitbasar@gmail.com>
2020-01-27 12:16:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,25 @@ sudo apt-get update
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 sudo dpkg-reconfigure locales
+echo "update done**************"
 sleep 10
 sudo apt-get install python3 -y
+echo "py3**************"
 sudo apt-get install jq -y
 sudo apt-get install python3-setuptools -y
+echo "py3 setup tools**************"
 sudo apt-get install python3-pip -y
+echo "pip3**************"
 sudo pip3 install pandas==0.23.4
+echo "pandas**************"
 sudo pip3 install selenium
+echo "selenium**************"
 sudo pip3 install beautifulsoup4
+echo "pip3**************"
 sudo pip3 install lxml
+echo "pip3**************"
 sudo pip3 install cassandra-driver
+echo "cass**************"
 sudo apt-get install firefox -y
+echo "firefox**************"
+
